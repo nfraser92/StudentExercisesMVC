258943118cd4759ac28e05d5ea5f3148e30c5c1a
Niall Fraser <niallfraser92@gmail.com>
2019-05-24 15:57:14 -0500

    CRUD on all resources. set up student repository

---
+++ StudentExercisesMVC/Repositories/StudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Repositories
+{
+    public class StudentRepository
+    {
+    }
+}
